5d01d0f45add67a90daa5a5ee27997f6502df8c6
ceoajproofile <ceoajproofile@gmail.com>
2026-04-14 15:06:01 +0800

    Update ExampleServiceExtension.kt

--- android/src/main/kotlin/com/example/my_java_bridge/ExampleServiceExtension.kt
+++ android/src/main/kotlin/com/example/my_java_bridge/ExampleServiceExtension.kt
@@ -1,21 +1,19 @@
 package com.example.my_java_bridge
 
+import android.content.Context
 import android.util.Log
-import com.onesignal.INotificationReceivedEvent
-import com.onesignal.INotificationServiceExtension
+import com.onesignal.notifications.INotificationReceivedEvent
+import com.onesignal.notifications.INotificationServiceExtension
 
 class ExampleServiceExtension : INotificationServiceExtension {
+    // In v5, the parameter type is INotificationReceivedEvent
     override fun onNotificationReceived(event: INotificationReceivedEvent) {
         val notification = event.notification
         val context = event.context
 
         Log.i("OneSignalExample", "Notification received: ${notification.body}")
 
-        // Example:
-        // event.preventDefault()
-        // Do custom work here
-        // notification.display()
-
-        // Let the notification continue displaying normally
+        // In v5, notifications display by default. 
+        // If you want to stop it, use event.preventDefault()
     }
 }
