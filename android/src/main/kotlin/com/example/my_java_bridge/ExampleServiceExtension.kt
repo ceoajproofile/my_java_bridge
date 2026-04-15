321c67f4345217c6b25f829b542e06df3b46a819
ceoajproofile <ceoajproofile@gmail.com>
2026-04-15 21:15:53 +0800

    Create ExampleServiceExtension.kt

---
+++ android/src/main/kotlin/com/example/my_java_bridge/ExampleServiceExtension.kt
@@ -0,0 +1,9 @@
+package com.example.my_java_bridge
+
+import android.util.Log
+
+class ExampleServiceExtension {
+    init {
+        Log.i("OneSignalExt", "Extension loaded successfully")
+    }
+}
