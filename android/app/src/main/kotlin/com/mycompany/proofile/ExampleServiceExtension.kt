4d1bea5342eee5c2a58ee8019b0feaa101a4e335
ceoajproofile <ceoajproofile@gmail.com>
2026-04-16 09:27:32 +0800

    Update ExampleServiceExtension.kt

--- android/app/src/main/kotlin/com/mycompany/proofile/ExampleServiceExtension.kt
+++ android/app/src/main/kotlin/com/mycompany/proofile/ExampleServiceExtension.kt
@@ -1,1 +1,17 @@
 
+package com.example.my_java_bridge
+
+import android.util.Log
+import com.onesignal.notifications.INotificationReceivedEvent
+import com.onesignal.notifications.INotificationServiceExtension
+
+class ExampleServiceExtension : INotificationServiceExtension {
+    override fun onNotificationReceived(event: INotificationReceivedEvent) {
+        val notification = event.notification
+        val data = notification.additionalData
+
+        Log.i("OneSignalExt", "Notification received: ${notification.body}")
+        Log.i("OneSignalExt", "Additional data: ${data?.toString()}")
+        Log.i("OneSignalExt", "notification_mode: ${data?.optString("notification_mode")}")
+    }
+}
